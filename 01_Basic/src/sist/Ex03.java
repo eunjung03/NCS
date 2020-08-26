1d0ce3c06597e3a0293e88a6556bf650061b2a6b
EunJung <venare36@naver.com>
2020-08-27 00:41:15 +0900

    8/24

---
+++ 01_Basic/src/sist/Ex03.java
@@ -0,0 +1,22 @@
+package sist;
+
+/*
+ * [문제] 아래와 같이 콘설 창에 출력해 보세요.
+ * 
+ * 이 름 : 임 정 훈
+ * 연락처 : 010-4725-3440
+ * 이메일 : jhehun@empas.com
+ * 주 소 : 경기도 고양시 덕양구
+ */
+public class Ex03 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("이  름 :\t임 정 훈");
+		System.out.print("연락처 :\t");
+		System.out.print("010-4725-3440\n");
+		System.out.print("이메일 :\tjhehun@empas.com");
+		System.out.print("\n주  소 :\t경기도 고양시 덕양구");
+	}
+
+}
---
+++ 02_Variable/src/sist/Ex04.java
@@ -0,0 +1,20 @@
+package sist;
+
+public class Ex04 {
+
+	public static void main(String[] args) {
+
+		// 단일문자형 : char형(0~65536) ==> 2byte
+		// 자바에서는 유니코드 체계로 단일문자가 처리가 됨.
+		// ==> C언어에서는  ASCII 코드 체계로 처리가 됨.
+		
+		char ch1 = '강';
+		System.out.println("ch1 >>> " + ch1);
+		
+		char ch2 = 'A';
+		System.out.println("ch2 >>> " + ch2);
+		
+		System.out.println((char)(ch2 + 1)); 	// 66값이 나타남
+	}
+
+}
